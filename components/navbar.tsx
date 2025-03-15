380604950d94f9ee0da6acb21b054f4022627629
Kroszborg <abhimanpanwar6@gmail.com>
2025-03-15 07:03:46 +0530

    update

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,15 +7,9 @@ import { ModeToggle } from "./mode-toggle";
 export default function Navbar({}): any {
   return (
     <nav className="fixed z-50 flex items-center justify-between w-full h-24 px-4 py-10 backdrop-blur-md bg-background bg-opacity-30 md:px-8 lg:px-12 xl:px-16 2xl:px-24">
-      <Link href="/">
-        <Image
-          alt="logo"
-          className="mb-2 cursor-pointer w-35 dark:invert"
-          src="/images/logo.svg"
-          height={100}
-          width={170}
-        />
-      </Link>
+    
+      <a href="/" className="text-xl font-bold">Metamorpher</a>
+    
       <div className="items-center hidden gap-2 md:flex">
         <ModeToggle />
       </div>
